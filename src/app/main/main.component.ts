b5747477a13b96e4e952d50697096339a96ec8b8
unknown <kirill_abramov@ukr.net>
2020-02-06 12:53:11 +0200

    cenzor

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+
+export class MainComponent implements OnInit {
+  badWords: Array<string> = [];
+  badWord: string = '';
+  allWordArray: Array<string> = [];
+  allWord: string = '';
+  placeholder = 'word here';
+  placeholder2 = 'text here';
+  color = 'lightskyblue';
+  color2 = 'lightskyblue';
+  badWordList: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addBadWords() {
+    if (this.badWord === '') {
+      this.color = 'red';
+      this.placeholder = 'Please, write a word';
+    }
+    else {
+      this.badWords.push(this.badWord);
+      this.badWordList = this.badWords.join(', ');
+      this.badWord = '';
+      this.color = 'lightskyblue';
+
+    }
+
+  }
+
+  addAllWords() {
+    if (this.allWord == '') {
+      this.color2 = 'red';
+      this.placeholder2 = 'Please, write some text';
+    }
+    else {
+      this.allWordArray = this.allWord.split(' ');
+      console.log(this.allWordArray);
+      for (let i = 0; i < this.allWordArray.length; i++) {
+        for (let j = 0; j < this.badWords.length; j++) {
+          if (this.allWordArray[i] == this.badWords[j]) {
+            this.allWordArray[i] = '*'.repeat(this.allWordArray[i].length);
+          }
+        }
+      }
+      this.allWord = this.allWordArray.join(' ');
+      this.color2 = 'lightskyblue';
+    }
+
+  }
+
+  reset() {
+    this.badWords.splice(0);
+    this.badWordList = '';
+    this.allWordArray.splice(0);
+    this.allWord = '';
+    this.placeholder = 'word here';
+    this.placeholder2 = 'text here';
+    this.color = 'lightskyblue';
+    this.color2 = 'lightskyblue';
+  }
+}
+
+
+
